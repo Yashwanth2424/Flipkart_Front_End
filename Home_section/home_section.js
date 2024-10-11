10e209acc794b21e693f78c3bfbfb41525a36954
Mini-project <thalkayashwanth1234@gmail.com>
2024-10-11 18:41:28 +0530

    RECORRECTED THE OVER ALL THE STYLE.CSS FILE

---
+++ Home_section/home_section.js
@@ -0,0 +1,13 @@
+// let inputEl = document.getElementById("navSearchInput")
+
+// inputEl.addEventListener("click",() => {
+//     inputEl.classList.add("border-none");
+// });
+
+
+
+// // let inputEl = document.getElementById("navSearchInput");
+
+// // inputEl.addEventListener("click", () => {
+// //     inputEl.classList.add("border-none");
+// // });
